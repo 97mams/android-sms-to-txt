e45d3bc67c27f4ccea91af3dbea9b300dae9f48b
97mams <97mams@gmail.com>
2026-02-07 12:14:28 +0300

    fix: Update minSdk version to support Android 5.0 and above

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.smsLogger"
-        minSdk = 26 // Supporte Android 8.0 et +
+        minSdk = 21 
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
